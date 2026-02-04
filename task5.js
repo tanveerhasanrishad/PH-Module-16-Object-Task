16f0b3e4aaec700b9cdb7745a4e589821f47d604
tanveerhasanrishad <tanveerrishad007@gmail.com>
2026-02-04 19:32:31 +0600

    completed task 5

---
+++ task5.js
@@ -0,0 +1,10 @@
+let myObject = {
+  name: "John Doe",
+  age: 25,
+  city: "Feni",
+  isStudent: true,
+};
+
+for (const prop in myObject) {
+  console.log("key: " + prop + " | " + "type: " + typeof myObject[prop]);
+}
